a0e756a29b622d3c996be24d676da24d7087a9d4
Remy <remy.sainsard@artsper.com>
2021-09-16 19:33:52 +0200

    deploy bash

---
+++ deploy_to_cdn.sh
@@ -0,0 +1,2 @@
+npx webpack --mode=production
+scp -P 42888 ./dist/main.js remysda@sshcloud.cluster024.hosting.ovh.net:./pixel-editor/main.js
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,3 @@ const pixelEditorManager = new PixelEditorManager();
 const pixelEditor = PixelEditorFactory.buildPixelEditor('pixel-editor', 10, 10);
 pixelEditorManager.init(pixelEditor);
 PixelToolFactory.buildPixelTool(pixelEditor);
-
